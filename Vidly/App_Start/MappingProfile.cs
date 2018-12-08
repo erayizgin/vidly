25ba912e93937fbb475478d49cbedda85c4754c1
eray izgin <erayizgin@gmail.com>
2018-12-08 22:08:56 +0300

    movies Dto and Datatable

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -14,6 +14,12 @@ namespace Vidly.App_Start
         {
             Mapper.CreateMap<Customer, CustomerDto>();
             Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MovieDto, Movie>();
+            Mapper.CreateMap<Genre, GenreDto>();
+
         }
     }
 }
---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public GenreDto Genre { get; set; }
+
+        public byte GenreId { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public byte NumberInStock { get; set; }
+        public byte NumberAvailable { get; set; }
+    }
+}
